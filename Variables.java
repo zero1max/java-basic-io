7cfb34a73eb10acbda51cf0e7894e9fc070d13f4
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-23 11:46:39 +0500

    Create Variables

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int yosh = 20;
+        double baho = 4.5;
+        char belgi = 'A';
+        boolean isStudent = true;
+        String ism = "Muhammad";
+
+        System.out.println("Yosh: " + yosh);
+        System.out.println("Baho: " + baho);
+        System.out.println("Belgi: " + belgi);
+        System.out.println("Talaba: " + isStudent);
+        System.out.println("Ism: " + ism);
+    }
+}

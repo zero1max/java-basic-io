756893b795ac27fe1cc7b3762e19903bc6295a79
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-23 11:55:41 +0500

    Create IfElseOperators

---
+++ IfElseOperators.java
@@ -0,0 +1,13 @@
+public class IfElseOperators {
+    public static void main(String[] args) {
+        int son = 10;
+
+        if (son > 0) {
+            System.out.println("Musbat son");
+        } else if (son < 0) {
+            System.out.println("Manfiy son");
+        } else {
+            System.out.println("Son nolga teng");
+        }
+    }
+}

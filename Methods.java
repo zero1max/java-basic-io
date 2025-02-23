5aeadec1b89775fa974631788cea410e093bbb77
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-23 18:18:50 +0500

    Create Methods

---
+++ Methods.java
@@ -0,0 +1,10 @@
+public class Methods {
+    // Funksiya
+    static void salomBer() {
+        System.out.println("Salom, do'stim!");
+    }
+
+    public static void main(String[] args) {
+        salomBer(); // Funksiyani chaqirish
+    }
+}

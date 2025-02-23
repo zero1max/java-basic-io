0c08be1213e1045279f0f741ebd11d71955538e8
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-23 18:20:01 +0500

    Update Methods

--- Methods.java
+++ Methods.java
@@ -1,9 +1,9 @@
 public class Methods {
-    // Funksiya
+    // Funksiya salomBer()
     static void salomBer() {
         System.out.println("Salom, do'stim!");
     }
-
+    // Funksiya salomBer() ni chaqirish
     public static void main(String[] args) {
         salomBer(); // Funksiyani chaqirish
     }

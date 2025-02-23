0847e91280f47b8a23f90841d54ee53ac2eb6aae
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-23 11:47:09 +0500

    Create Plus minus Operators

---
+++ Plus_minus_operators.java
@@ -0,0 +1,11 @@
+public class Plus_minus_operators {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+
+        System.out.println("Qo‘shish: " + (a + b));
+        System.out.println("Ayirish: " + (a - b));
+        System.out.println("Ko‘paytirish: " + (a * b));
+        System.out.println("Bo‘lish: " + (a / b));
+        System.out.println("Qoldiq: " + (a % b));
+    }
+}

3d2018f24edb382eac2c4b5adae2cec15ff9947f
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-23 11:56:02 +0500

    Create Switch

---
+++ Switch.java
@@ -0,0 +1,19 @@
+public class Switch {
+    public static void main(String[] args) {
+        int kun = 3;
+
+        switch (kun) {
+            case 1:
+                System.out.println("Dushanba");
+                break;
+            case 2:
+                System.out.println("Seshanba");
+                break;
+            case 3:
+                System.out.println("Chorshanba");
+                break;
+            default:
+                System.out.println("Noma’lum kun");
+        }
+    }
+}

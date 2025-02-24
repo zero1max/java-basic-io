35df86541bc63b6c4dceed0f4edd2dece48cb61f
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-24 16:34:34 +0500

    Create Begin1

---
+++ Algorithms/Begin/Begin1.java
@@ -0,0 +1,14 @@
+package Algorithms.Begin;
+
+import java.util.Scanner;
+
+public class Begin1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kvadrat tomonini kiriting: ");
+        float a = scanner.nextFloat();
+
+        float P = 4*a;
+        System.out.print("Uning perimetri: "+P);
+    }
+}

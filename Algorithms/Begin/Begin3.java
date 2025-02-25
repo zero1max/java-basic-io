d9c8f9317f44e378717c17c765aa9d1338ddbe2a
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-25 10:33:57 +0500

    Create Begin3

---
+++ Algorithms/Begin/Begin3.java
@@ -0,0 +1,18 @@
+package Algorithms.Begin;
+
+import java.util.Scanner;
+
+public class Begin3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a tomonni kiriting: ");
+        float a = scanner.nextFloat();
+        System.out.print("b tomonni kiriting: ");
+        float b = scanner.nextFloat();
+
+        float P = 2*(a+b);
+        float S = a*b;
+        System.out.println("Perimetri: "+P);
+        System.out.println("Yuzasi: "+S);
+    }
+}

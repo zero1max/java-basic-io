dadc99a258c4aeb516431beaf82a4a8fbf43e2b5
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-25 10:34:12 +0500

    Create Begin4

---
+++ Algorithms/Begin/Begin4.java
@@ -0,0 +1,16 @@
+package Algorithms.Begin;
+
+import java.util.Scanner;
+
+public class Begin4 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("d kiriting: ");
+        float d = scanner.nextFloat();
+        double pi = 3.14;
+        double L = pi * d;
+
+        System.out.println("Uzunligi: "+L);
+    }
+}

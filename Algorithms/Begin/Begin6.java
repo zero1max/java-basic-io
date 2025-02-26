f3d4a89bfd6c30142c22e46e69ef8c8c65dd7bbb
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-26 11:31:33 +0500

    Create Begin6

---
+++ Algorithms/Begin/Begin6.java
@@ -0,0 +1,22 @@
+package Algorithms.Begin;
+
+import java.util.Scanner;
+
+public class Begin6 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("a qirrasi: ");
+        float a = sc.nextFloat();
+        System.out.print("b qirrasi: ");
+        float b = sc.nextFloat();
+        System.out.print("c qirrasi: ");
+        float c = sc.nextFloat();
+
+        double V = a*b*c;
+        double S = 2*(a*b+b*c+a*c);
+
+        System.out.println("Hajmi: "+V);
+        System.out.println("Yuzasi: "+S);
+    }
+}

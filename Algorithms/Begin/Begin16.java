284d26e5e54104a63b7a86337f6a427906f83136
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-26 12:10:26 +0500

    Create Begin16

---
+++ Algorithms/Begin/Begin16.java
@@ -0,0 +1,17 @@
+package Algorithms.Begin;
+
+import java.util.Scanner;
+
+public class Begin16 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("x1: ");
+        float x1 = sc.nextFloat();
+        System.out.print("x2: ");
+        float x2 = sc.nextFloat();
+
+        double Distance = Math.abs(x2-x1);
+        System.out.println("Distance: "+Distance);
+    }
+}

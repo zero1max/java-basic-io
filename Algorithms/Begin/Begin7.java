453632cf9651bfd541be6050ff031832019a1a81
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-26 11:31:49 +0500

    Create Begin7

---
+++ Algorithms/Begin/Begin7.java
@@ -0,0 +1,19 @@
+package Algorithms.Begin;
+
+import java.util.Scanner;
+
+public class Begin7 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("R: ");
+        float R = sc.nextFloat();
+        double pi = 3.14;
+
+        double L =  2*pi*R;
+        double S =  pi * (float)Math.pow(R, 2);
+
+        System.out.println("Uzunligi: "+L);
+        System.out.println("Yuzasi: "+S);
+    }
+}

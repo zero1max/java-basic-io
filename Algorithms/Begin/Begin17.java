242415f4ae08bcae81457b15f16695a9f5c28020
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-02-26 12:10:42 +0500

    Create Begin17

---
+++ Algorithms/Begin/Begin17.java
@@ -0,0 +1,22 @@
+package Algorithms.Begin;
+
+import java.util.Scanner;
+
+public class Begin17 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("A nuqta: ");
+        float A = sc.nextFloat();
+        System.out.print("B nuqta: ");
+        float B = sc.nextFloat();
+        System.out.print("C nuqta: ");
+        float C = sc.nextFloat();
+
+        double AC = Math.abs(C-A);
+        double BC = Math.abs(C-B);
+
+        System.out.println("AC: "+AC);
+        System.out.println("BC: "+BC);
+    }
+}

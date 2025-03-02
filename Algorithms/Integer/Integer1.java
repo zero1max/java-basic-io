89f7e3cfbcf994b3657ad996e96a2d7688b1fe62
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-03-02 22:35:38 +0500

    Create Integer1

---
+++ Algorithms/Integer/Integer1.java
@@ -0,0 +1,15 @@
+package Algorithms.Integer;
+
+import java.util.Scanner;
+
+public class Integer1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masofa kiriting(sm): ");
+        float sm = sc.nextInt();
+
+        double m = sm/100;
+        System.out.println("Metr: "+m);
+    }
+}

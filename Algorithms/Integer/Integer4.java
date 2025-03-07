f8dff8b7295b9cbc6f1242e41400e29e13d631c5
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-03-07 11:18:17 +0500

    Create Integer4

---
+++ Algorithms/Integer/Integer4.java
@@ -0,0 +1,19 @@
+package Algorithms.Integer;
+
+import java.util.Scanner;
+
+public class Integer4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("A: ");
+        int a = sc.nextInt();
+        System.out.print("B: ");
+        int b = sc.nextInt();
+
+        int N = a/b;
+        System.out.println("N: "+N);
+
+        sc.close();
+    }
+}
